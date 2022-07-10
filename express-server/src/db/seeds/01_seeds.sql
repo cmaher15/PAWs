1a395e09e87a5798ff7ff4ec483dcf9e0e582008
Markjust1 <novikov711@gmail.com>
2022-07-09 22:32:28 -0600

    fix erd and add breed_incomp column

--- express-server/src/db/seeds/01_seeds.sql
+++ express-server/src/db/seeds/01_seeds.sql
@@ -15,26 +15,26 @@ VALUES
 
 
 INSERT INTO dogs 
-  (name, breed, gender, age, size, reactive, good_with_people,size_compatibility, gender_compatibility, description, photo, owner_id) 
+  (name, breed, gender, age, size, reactive, good_with_people,size_compatibility, gender_compatibility, breed_incompatibility, description, photo, owner_id) 
   VALUES 
-  ('Spot', 'pitbull', 'male', 3, 'medium', false, true, ARRAY ['small', 'middle'], ARRAY ['male', 'female'], 'Very playful and kind to people and kids, Spot loves to spend time outdoors', 'https://imgur.com/sQsLVbu', 1),
+  ('Spot', 'pitbull', 'male', 3, 'medium', false, true, ARRAY ['small', 'middle'], ARRAY ['male', 'female'], ARRAY [],'Very playful and kind to people and kids, Spot loves to spend time outdoors', 'https://imgur.com/sQsLVbu', 1),
 
-  ('Bailey', 'poodle', 'female', 2, 'medium', false, true, ARRAY ['small', 'middle', 'large'], ARRAY ['male', 'female'], 'Smart and happy dog, enjoys playing with everyone', 'https://imgur.com/vqCeSBl', 2),
+  ('Bailey', 'poodle', 'female', 2, 'medium', false, true, ARRAY ['small', 'middle', 'large'], ARRAY ['male', 'female'], ARRAY ['pitbull'], 'Smart and happy dog, enjoys playing with everyone', 'https://imgur.com/vqCeSBl', 2),
 
-  ('Flash', 'pug', 'male', 7, 'small', true, true, ARRAY ['small'], ARRAY ['male', 'female'], 'Small but quick, Flash loved dog parks more than anything, a little cautious of other dogs', 'https://imgur.com/1GUzYh9', 3),
+  ('Flash', 'pug', 'male', 7, 'small', true, true, ARRAY ['small'], ARRAY ['male', 'female'],ARRAY ['german shepherd'], 'Small but quick, Flash loved dog parks more than anything, a little cautious of other dogs', 'https://imgur.com/1GUzYh9', 3),
 
-  ('Gatsby', 'labrador', 'male', 5, 'medium', true, false, ARRAY ['middle'], ARRAY ['male', 'female'], 'Being a resque dog, Gatsby is a sweet and loving dog, but needs a little time to get adjusted to new faces', 'https://imgur.com/vrmjdeR', 4),
+  ('Gatsby', 'labrador', 'male', 5, 'medium', true, false, ARRAY ['middle'], ARRAY ['male', 'female'], ARRAY [], 'Being a resque dog, Gatsby is a sweet and loving dog, but needs a little time to get adjusted to new faces', 'https://imgur.com/vrmjdeR', 4),
 
-  ('Abby', 'bulldog', 'female', 4, 'medium', true, true, ARRAY ['middle'], ARRAY ['female'], 'Give Abby a treat, and she will become your best friend', 'https://imgur.com/nexY1Ro', 5),
+  ('Abby', 'bulldog', 'female', 4, 'medium', true, true, ARRAY ['middle'], ARRAY ['female'],ARRAY ['pitbull', 'doberman'], 'Give Abby a treat, and she will become your best friend', 'https://imgur.com/nexY1Ro', 5),
 
-  ('Jackson', 'german shepherd', 'male', 7, 'large', true, true, ARRAY ['middle', 'large'], ARRAY ['male', 'female'], 'As a resque dog, Jackson is a bit careful towards other dogs at first, but given a little time, he becomes best friends with everybody', 'https://imgur.com/7r7mbKl', 6),
+  ('Jackson', 'german shepherd', 'male', 7, 'large', true, true, ARRAY ['middle', 'large'], ARRAY ['male', 'female'], ARRAY [], 'As a resque dog, Jackson is a bit careful towards other dogs at first, but given a little time, he becomes best friends with everybody', 'https://imgur.com/7r7mbKl', 6),
 
-  ('Grace', 'pug', 'female', 1, 'small', false, true, ARRAY ['small'], ARRAY ['male', 'female'], 'Absolutely loving dog, I hope you love her too', 'https://imgur.com/Jgjm6VU', 7),
+  ('Grace', 'pug', 'female', 1, 'small', false, true, ARRAY ['small'], ARRAY ['male', 'female'], ARRAY [], 'Absolutely loving dog, I hope you love her too', 'https://imgur.com/Jgjm6VU', 7),
 
-  ('Percy', 'boxer', 'male', 5, 'large', false, true, ARRAY ['middle', 'large'], ARRAY ['male', 'female'], 'Large dog who thinks he is a puppy, Percy loves long walks and rubs on his back', 'https://imgur.com/C7NvzdK', 8),
+  ('Percy', 'boxer', 'male', 5, 'large', false, true, ARRAY ['middle', 'large'], ARRAY ['male', 'female'], ARRAY [], 'Large dog who thinks he is a puppy, Percy loves long walks and rubs on his back', 'https://imgur.com/C7NvzdK', 8),
 
-  ('Daisy', 'pitbull', 'female', 2, 'medium', true, false, ARRAY ['small', 'middle'], ARRAY ['female'], 'Resque dog, quite careful around other dogs and people, just give her a treat and she will become best friends with you', 'https://imgur.com/4vc6oNS', 9),
+  ('Daisy', 'pitbull', 'female', 2, 'medium', true, false, ARRAY ['small', 'middle'], ARRAY ['female'], ARRAY ['rottweiler', 'pitbull'], 'Resque dog, quite careful around other dogs and people, just give her a treat and she will become best friends with you', 'https://imgur.com/4vc6oNS', 9),
 
-  ('Mabel', 'german shepherd', 'female', 2, 'large', false, true, ARRAY ['small', 'middle', 'large'], ARRAY ['female'], 'Very obedient and well behaved, Mabel lacks good dog friendships, she loves going to the beach and dog parks', 'https://imgur.com/JuOwJTW', 10),
+  ('Mabel', 'german shepherd', 'female', 2, 'large', false, true, ARRAY ['small', 'middle', 'large'], ARRAY ['female'], ARRAY [], 'Very obedient and well behaved, Mabel lacks good dog friendships, she loves going to the beach and dog parks', 'https://imgur.com/JuOwJTW', 10),
 
-  ('Arnold', 'boxer', 'male', 4, 'large', false, true, ARRAY['small', 'middle'], ARRAY ['male', 'female'], 'Very energetic and playful, Arnord will not leave anyone without a smile', 'https://imgur.com/p6XnnU8', 11);
+  ('Arnold', 'boxer', 'male', 4, 'large', false, true, ARRAY['small', 'middle'], ARRAY ['male', 'female'], ARRAY ['poodle'], 'Very energetic and playful, Arnord will not leave anyone without a smile', 'https://imgur.com/p6XnnU8', 11);
--- express-server/src/routes/dogs.js
+++ express-server/src/routes/dogs.js
@@ -14,3 +14,11 @@ module.exports = db => {
 
   return router;
 };
+
+
+// CRUD
+
+// CREATE = POST /dogs/:id   create a dog
+// READ   = GET  /dogs       get matched dogs
+// UPDATE = POST /dogs/:id   update dogs' info
+// DELETE = POST /dogs/:id   delete user
