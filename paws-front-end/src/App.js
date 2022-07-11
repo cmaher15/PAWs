a47561fea1a506db915e16784b2a193d5e5362fa
cmaher15 <ceilidhemaher@hotmail.com>
2022-07-11 11:38:40 -0700

    Delete unused HandleOnSubmit component

--- paws-front-end/src/App.js
+++ paws-front-end/src/App.js
@@ -11,7 +11,6 @@ import Login from "./components/Login";
 import AboutUs from "./components/AboutUs";
 import Footer from "./components/Footer";
 import Terms from "./components/Terms";
-import HandleOnSubmit from "./components/HandleOnSubmit";
 
 // Helpers
 import {
