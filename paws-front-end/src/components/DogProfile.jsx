618e86199169fa988c7c4eacc45b24157f948bd6
cmaher15 <ceilidhemaher@hotmail.com>
2022-07-12 00:12:38 -0700

    implemented hover feature for icons

--- paws-front-end/src/components/DogProfile.jsx
+++ paws-front-end/src/components/DogProfile.jsx
@@ -1,20 +1,32 @@
 import React from "react";
-import '../styles/DogProfile.scss'
+import "../styles/DogProfile.scss";
 
 export default function DogProfile() {
   return (
     <main className="dogProfile">
-    <div>
-      <img className="dog"
-      src="images/happy_baby.jpeg"/>
-    </div>
-    <h2>Rocky</h2>
-    <p>Rocky is a good boy who likes jumping in puddles! He would absolutely love to go on a walk with you.</p>
-    <p className="icons">
-    <i className="fa-solid fa-location-dot fa-xl" id="location"></i>
-    <i className="fa-solid fa-heart fa-xl" id="heart"></i>
-    <i className="fa-solid fa-message fa-xl" id="chat"></i>
-    </p>
-  </main>
-  )
+      <div>
+        <img className="dog" src="images/happy_baby.jpeg" />
+      </div>
+      <h2>Rocky</h2>
+      <p>
+        Rocky is a good boy who likes jumping in puddles! He would absolutely
+        love to go on a walk with you.
+      </p>
+      <p className="icons">
+        <i className="fa-solid fa-location-dot fa-xl"
+         onMouseOver={({ target }) => (target.style.color = "black")}
+         onMouseOut={({ target }) => (target.style.color = "gray")}></i>
+        <i
+          className="fa-solid fa-heart fa-xl"
+          onMouseOver={({ target }) => (target.style.color = "red")}
+          onMouseOut={({ target }) => (target.style.color = "gray")}
+        ></i>
+        <i
+          className="fa-solid fa-message fa-xl"
+          onMouseOver={({ target }) => (target.style.color = "blue")}
+          onMouseOut={({ target }) => (target.style.color = "gray")}
+        ></i>
+      </p>
+    </main>
+  );
 }
