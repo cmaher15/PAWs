92e6963e9571e46fafaa605ae33bf41b31dc54ef
cmaher15 <ceilidhemaher@hotmail.com>
2022-07-11 23:58:09 -0700

    more formatting on dogprofile component

--- paws-front-end/src/components/DogProfile.jsx
+++ paws-front-end/src/components/DogProfile.jsx
@@ -10,7 +10,11 @@ export default function DogProfile() {
     </div>
     <h2>Rocky</h2>
     <p>Rocky is a good boy who likes jumping in puddles! He would absolutely love to go on a walk with you.</p>
-
+    <p className="icons">
+    <i className="fa-solid fa-location-dot fa-xl" id="location"></i>
+    <i className="fa-solid fa-heart fa-xl" id="heart"></i>
+    <i className="fa-solid fa-message fa-xl" id="chat"></i>
+    </p>
   </main>
   )
 }
