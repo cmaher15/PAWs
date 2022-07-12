0d3cba351c9b1340467e9a4c61d7daa15884c78a
cmaher15 <ceilidhemaher@hotmail.com>
2022-07-12 00:18:38 -0700

    updated icons

--- paws-front-end/src/components/DogProfile.jsx
+++ paws-front-end/src/components/DogProfile.jsx
@@ -7,12 +7,12 @@ export default function DogProfile() {
       <div>
         <img className="dog" src="images/happy_baby.jpeg" />
       </div>
-      <h2>Rocky</h2>
+      <h2>Rocky || Male || Pitbull || 3 1/2</h2>
       <p>
         Rocky is a good boy who likes jumping in puddles! He would absolutely
         love to go on a walk with you.
       </p>
-      <p className="icons">
+      <span className="icons">
         <i className="fa-solid fa-location-dot fa-xl"
          onMouseOver={({ target }) => (target.style.color = "black")}
          onMouseOut={({ target }) => (target.style.color = "gray")}></i>
@@ -26,7 +26,7 @@ export default function DogProfile() {
           onMouseOver={({ target }) => (target.style.color = "blue")}
           onMouseOut={({ target }) => (target.style.color = "gray")}
         ></i>
-      </p>
+      </span>
     </main>
   );
 }
