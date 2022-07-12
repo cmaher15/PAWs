08baa26758de8185e9168f9dd3246cba3714b798
cmaher15 <ceilidhemaher@hotmail.com>
2022-07-11 22:57:22 -0700

    play around more with filestack, get nowhere

--- paws-front-end/src/components/DogForm/Image.jsx
+++ paws-front-end/src/components/DogForm/Image.jsx
@@ -1,21 +1,21 @@
 import React from "react";
+// import * as filestack from "filestack-js";
 
 export default function Image(props) {
-  // const client = filestack.init('AiisZJhdbQK2bEQwvqRqKz')
-  // client.picker().open();
+  // const client = filestack.init("AiisZJhdbQK2bEQwvqRqKz");
+  // client.picker().open()
   return (
     <div className="component">
-    <label className="title">
-      Image: 
-    </label>
-    <br />
-    <br />
-    Please upload an image of your dog. This image will be used on their
-    profile and visible to other users.
-    <br />
-    <br />
-    <script src="//static.filestackapi.com/filestack-js/3.x.x/filestack.min.js"></script>
-    <input
+      <label className="title">Image:</label>
+      <br />
+      <br />
+      Please upload an image of your dog. This image will be used on their
+      profile and visible to other users.
+      <br />
+      <br />
+      {/* <input type="file" />
+      <script src="//static.filestackapi.com/filestack-js/3.x.x/filestack.min.js"></script> */}
+      <input
       type="file"
       id="myFile"
       name="photo"
@@ -23,6 +23,6 @@ export default function Image(props) {
       onChange={(event) => props.onChange(event.target.value)}
       required
     ></input>
-  </div>
+    </div>
   );
 }
