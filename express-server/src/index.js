3f248c24cd5c6e10ff6fd7c93c98fc420e3e37c7
cmaher15 <ceilidhemaher@hotmail.com>
2022-07-12 19:21:47 -0700

    move chat server code to proper file, comment out for now

--- express-server/src/index.js
+++ express-server/src/index.js
@@ -2,10 +2,6 @@ const PORT = process.env.PORT || 8001;
 const ENV = require("./environment");
 const app = require("./application")(ENV);
 const server = require("http").Server(app);
-// const express = require("express");
-// const app = express();
-// const sock = require("socket.io");
-// // const client = require("./db");
 
 // const WebSocket = require("ws");
 // const wss = new WebSocket.Server({ server });
@@ -20,41 +16,6 @@ const server = require("http").Server(app);
 //   };
 // });
 
-// const http =
 server.listen(PORT, () => {
   console.log(`Listening on port ${PORT} in ${ENV} mode`);
 });
-
-// const clients = {};
-// console.log("socketio.Server", sock.Server);
-// const io = new sock.Server(http);
-
-// io.on("connection", (client) => {
-//   const name = client;
-//   console.log("Someone connected!", client.id, name);
-//   client.name = name;
-//   clients[name] = client.id;
-//   console.log(clients);
-
-//   client.broadcast.emit("server", `${name}: just connected`);
-
-//   client.emit("name", name);
-
-//   client.on("message", (data) => {
-//     console.log("message:", data);
-//     data.from = client.name;
-
-//     if (data.to) {
-//       const id = clients[data.to];
-//       console.log("message is for: ", data.to, id);
-//       io.to(id).emit("user", data);
-//       return;
-//     }
-//     client.broadcast.emit("user", data);
-//   });
-
-//   client.on("disconnect", () => {
-//     delete clients[client.name];
-//     console.log("owner Disconnected!", client.name);
-//   });
-// });
