35887475a630c162335e43d3a30229215c6c0dbd
Eugene Helland <eugenehelland@mail.com>
2022-07-12 22:09:55 -0600

    Change "res.status(403)" in the catch block of /login, in owners.js, to "res.sendStatus", an error message is now show in the console on invalid email.

--- paws-front-end/src/components/HeaderLoggedOut.jsx
+++ paws-front-end/src/components/HeaderLoggedOut.jsx
@@ -24,7 +24,7 @@ const HeaderLoggedOut = function (props) {
           props.setUserId(response.id);
         })
         .catch(error => {
-          console.log(error);
+          console.log(error.message);
           // Display some sort of error message to user?
           alert("Login Unsuccessful");
         });
