762a23d9dd65d6aa1f5d06a378e72b6d745f3ed7
cmaher15 <ceilidhemaher@hotmail.com>
2022-07-14 21:40:38 -0700

    begin styling header

--- paws-front-end/src/App.js
+++ paws-front-end/src/App.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import axios from "axios";
-import "./App.css";
+
 
 // Components
 import Header from "./components/Header";
--- paws-front-end/src/components/Header.jsx
+++ paws-front-end/src/components/Header.jsx
@@ -2,16 +2,17 @@ import React from "react";
 import { Link } from "react-router-dom";
 import HeaderLoggedIn from "./HeaderLoggedIn";
 import HeaderLoggedOut from "./HeaderLoggedOut";
+import "../styles/Header.css"
 
 const Header = function (props) {
   return (
     <div className="header-div">
-      <h1>
+      <h1 className="home">
         <Link to={"/"} onClick={() => props.setUrlPath("/")}>
           PAWs
         </Link>
       </h1>
-      <span className="title-descrition">Pups. Awaiting. Walks.</span>
+      <span className="title-description">Pups. Awaiting. Walks.</span>
 
       {props.loggedIn ? (
         <HeaderLoggedIn
