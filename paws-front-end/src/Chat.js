126f0ad048a882b34f5a0c6bd68dbb63055aa0c8
cmaher15 <ceilidhemaher@hotmail.com>
2022-07-15 12:27:38 -0700

    begin adding user names into chat

--- paws-front-end/src/Chat.js
+++ paws-front-end/src/Chat.js
@@ -2,13 +2,27 @@ import { useEffect, useState } from "react";
 import io from "socket.io-client";
 import { v4 } from "uuid";
 import "./styles/Chat.css";
+import axios from "axios";
 
-export default function Chat() {
+export default function Chat(props) {
   const [name, setName] = useState("");
   const [messages, setMessages] = useState([]);
   const [text, setText] = useState("");
   const [to, setTo] = useState("");
   const [socket, setSocket] = useState();
+  console.log("cookie", document.cookie);
+  const userID = document.cookie.slice(7);
+  console.log("userId", userID);
+
+  axios
+    .get(`/api/owners/${userID}`)
+    .then((response) => {
+      console.log("response for user:", response);
+      console.log("username", response.data.name);
+      setName(response.data.name);
+      return response;
+    })
+    .catch((err) => console.error(err));
 
   useEffect(() => {
     // Connect to server
@@ -27,8 +41,8 @@ export default function Chat() {
       setMessages((prev) => [msg, ...prev]);
     });
 
-    socket.on("name", (data) => {
-      setName(data);
+    socket.on("name", (name) => {
+      setName(name);
     });
 
     return () => socket.disconnect(); // prevents memory leak!
