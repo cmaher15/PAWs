29c00f43f51038cd1c0659997cadbb54b36dc678
Eugene Helland <eugenehelland@mail.com>
2022-07-16 15:12:25 -0600

    Fix FetchProfiles.jsx

--- paws-front-end/src/components/fetchProfiles.jsx
+++ paws-front-end/src/components/fetchProfiles.jsx
@@ -1,29 +1,52 @@
 import React, { useState, useEffect } from "react";
+import axios from "axios";
+import DogMatches from "./DogMatches";
+import UsersDogs from "./UsersDogs";
 
 // This component exists to fetch owners/dogs from server and pass results as
 // props to usersDogs, and matchDogs
-const fetchProfiles = function () {
-  const [isLoading, setIsLoading] = useState(true);
-  const [areaDogs, setAreaDogs] = useState(true);
+const fetchProfiles = function (props) {
+  const [isLoadingDogs, setIsLoadingDogs] = useState(true);
+  const [isLoadingOwners, setIsLoadingOwners] = useState(true);
+  const [areaDogs, setAreaDogs] = useState();
+  const [areaOwners, setAreaOwners] = useState();
 
-  const callSetAreaDogs = dogs => setAreaDogs(dogs);
+  // const callSetAreaDogs = dogs => setAreaDogs(dogs);
 
+  // Fetch dogs from server
   useEffect(() => {
     axios
       .get(`/api/dogs`)
       .then(response => {
-        console.log("response in fetchAreaDogs: ", response.data);
-        callSetAreaDogs(response.data);
-        setIsLoading(false);
+        // callSetAreaDogs(response.data);
+        setAreaDogs(response.data);
+        setIsLoadingDogs(false);
       })
       .catch(error => console.log("Error fetching dogs from server: ", error));
   }, []);
 
-  if (isLoading) {
+  // Fetch owners from server
+  useEffect(() => {
+    axios
+      .get(`/api/owners`)
+      .then(response => {
+        // callSetAreaDogs(response.data);
+        setAreaOwners(response.data);
+        setIsLoadingOwners(false);
+      })
+      .catch(error => console.log("Error fetching dogs from server: ", error));
+  }, []);
+
+  if (isLoadingDogs || isLoadingOwners) {
     // Shows loading whilst waiting for axios request (gif dog goes here)
     return <p>Loading...</p>;
   } else {
-    return;
+    console.log("props.urlPath: ", props.urlPath);
+    return props.urlPath === "/dog-matches" ? (
+      <DogMatches areaDogs={areaDogs} areaOwners={areaOwners} />
+    ) : (
+      <UsersDogs areaDogs={areaDogs} />
+    );
   }
 };
 
