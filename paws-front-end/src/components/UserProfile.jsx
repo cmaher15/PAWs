c097478815e6e07f85ddc779d22a73ab9e3f85c8
cmaher15 <ceilidhemaher@hotmail.com>
2022-07-16 10:55:27 -0700

    change logged out menu to include about us and terms

--- paws-front-end/src/components/UserProfile.jsx
+++ paws-front-end/src/components/UserProfile.jsx
@@ -1,59 +1,75 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import UsersDogList from "./UserForm/UserDogList";
-import "../styles/Header.css"
+import "../styles/Header.css";
 
 const UserProfile = function (props) {
   return (
     <span>
-    {props.loggedIn ? ( <div className='dropdown'>
-    <button className='dropbtn'><p>Menu</p></button>
-    <div className='dropdown-content'>
-      <button className='mainbtn'>
-        <Link
-          className='hovermenu'
-          to={"/users-dogs"}
-          onClick={() => props.setUrlPath("/users-dogs")}
-        >
-          <p className='dropdown-text'>Manage Your Dogs</p>
-        </Link>
-      </button>
-      <button className='mainbtn'>
-        {" "}
-        <Link
-        className='hovermenu'
-          to={"/dog-matches"}
-          onClick={() => props.setUrlPath("/dog-matches")}
-        >
-          <p className='dropdown-text'>View Your Matches</p>
-        </Link>
-      </button>
-    </div>
-  </div>) : (    <div className='dropdown'>
-      <button className='dropbtn'><p>Menu</p></button>
-      <div className='dropdown-content'>
-        <button className='mainbtn'>
-          <Link
-            className='hovermenu'
-            to={"/login"}
-            onClick={() => props.setUrlPath("/login")}
-          >
-            <p className='dropdown-text'>Login</p>
-          </Link>
-        </button>
-        <button className='mainbtn'>
-          {" "}
-          <Link
-          className='hovermenu'
-            to={"/register-user"}
-            onClick={() => props.setUrlPath("/register-user")}
-          >
-            <p className='dropdown-text'>Register</p>
-          </Link>
-        </button>
-      </div>
-    </div>)}
-
+      {props.loggedIn ? (
+        <div className="dropdown">
+          <button className="dropbtn">
+            <p>Menu</p>
+          </button>
+          <div className="dropdown-content">
+            <button className="mainbtn">
+              <Link
+                className="hovermenu"
+                to={"/users-dogs"}
+                onClick={() => props.setUrlPath("/users-dogs")}
+              >
+                <p className="dropdown-text">Manage Your Dogs</p>
+              </Link>
+            </button>
+            <button className="mainbtn">
+              {" "}
+              <Link
+                className="hovermenu"
+                to={"/dog-matches"}
+                onClick={() => props.setUrlPath("/dog-matches")}
+              >
+                <p className="dropdown-text">View Your Matches</p>
+              </Link>
+            </button>
+          </div>
+        </div>
+      ) : (
+        <div className="dropdown">
+          <button className="dropbtn">
+            <p>Menu</p>
+          </button>
+          <div className="dropdown-content">
+            <button className="mainbtn">
+              {" "}
+              <Link
+                className="hovermenu"
+                to={"/register-user"}
+                onClick={() => props.setUrlPath("/register-user")}
+              >
+                <p className="dropdown-text">Register</p>
+              </Link>
+            </button>
+            <button className="mainbtn">
+              <Link
+                className="hovermenu"
+                to={"/about-us"}
+                onClick={() => props.setUrlPath("/about-us")}
+              >
+                <p className="dropdown-text">About Us</p>
+              </Link>
+            </button>
+            <button className="mainbtn">
+              <Link
+                className="hovermenu"
+                to={"/terms"}
+                onClick={() => props.setUrlPath("/terms")}
+              >
+                <p className="dropdown-text">Terms</p>
+              </Link>
+            </button>
+          </div>
+        </div>
+      )}
     </span>
   );
 };
