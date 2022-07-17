028373cc5c4f86864ae51b45d7b026d1886f8103
cmaher15 <ceilidhemaher@hotmail.com>
2022-07-16 22:12:24 -0700

    begin work on profile page

--- paws-front-end/src/components/LandingPage.jsx
+++ paws-front-end/src/components/LandingPage.jsx
@@ -2,11 +2,16 @@ import React from "react";
 import "../styles/LandingPage.css";
 
 export default function LandingPage () {
+
   return (
-    <div className='landing-page'>
+    <main className="ownerProfile">
+       <div className="user-photo"><img className="userpic" src="images/ceilidhe.jpeg"/></div>
       <h3>My Favourites</h3>
       <h3>My Matches</h3>
       <h3>My Dogs</h3>
-    </div>
-  )
-}
+      <span className="locationInfo">
+        <i className="fa-solid fa-location-dot fa-2xl"></i>
+        <p className="distance">Powell River, BC</p>
+      </span>
+    </main>
+  )}
