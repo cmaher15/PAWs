e2fcdddd26d8e57a9218f80bae1592f95f7b4c02
Markjust1 <novikov711@gmail.com>
2022-07-18 11:39:02 -0600

    css improv

--- express-server/src/routes/favourites.js
+++ express-server/src/routes/favourites.js
@@ -16,7 +16,7 @@ module.exports = (db) => {
   router.get("/favourites/:id", (req, res) => {
     const id = req.params.id;
     db.query(
-      `SELECT dogs.name as dogs_name, dogs.breed as breed, dogs.gender as gender, dogs.age as age, dogs.size as size, dogs.reactive as reactive, dogs.good_with_reactive_dogs as good_with_reactive_dogs, dogs.size_compatibility as size_compatibility, dogs.gender_compatibility as gender_compatibility, dogs.breed_incompatibility as breed_incompatibility, dogs.description as description, dogs.photo_url as dogs_photo, owners.id as owners_id, owners.name as owners_name, owners.city as city, owners.thumbnail_photo_url as owners_photo FROM dogs JOIN owners ON dogs.owner_id = owners.id JOIN favourites ON dogs.id = favourites.dog_id WHERE favourites.owner_id = ${id}`
+      `SELECT dogs.name as name, dogs.breed as breed, dogs.gender as gender, dogs.age as age, dogs.size as size, dogs.reactive as reactive, dogs.good_with_reactive_dogs as good_with_reactive_dogs, dogs.size_compatibility as size_compatibility, dogs.gender_compatibility as gender_compatibility, dogs.breed_incompatibility as breed_incompatibility, dogs.description as description, dogs.photo_url as photo_url, owners.id as owners_id, owners.name as owners_name, owners.city as city, owners.thumbnail_photo_url as owners_photo FROM dogs JOIN owners ON dogs.owner_id = owners.id JOIN favourites ON dogs.id = favourites.dog_id WHERE favourites.owner_id = ${id}`
     )
       .then((result) => {
         res.send(result.rows);
