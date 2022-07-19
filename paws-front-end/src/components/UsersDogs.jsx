6dd6ebaae3e4a67898eb3757434386e9cb4c83b9
cmaher15 <ceilidhemaher@hotmail.com>
2022-07-19 13:27:19 -0700

    add conditional rendering for user dogs

--- paws-front-end/src/components/UsersDogs.jsx
+++ paws-front-end/src/components/UsersDogs.jsx
@@ -5,8 +5,6 @@ import "../styles/Header.css";
 import { UserDogProfileCard } from "../helpers/UserDogProfileCard";
 
 const UsersDogs = function (props) {
-  // console.log("UserDogs props.areaDogs", props.areaDogs);
-  // console.log("UsersDogs ownerId", props.userId);
   const myDogs = [];
   props.areaDogs.map(dog => {
     if (dog.owner_id == window.localStorage.getItem("paws_id")) {
@@ -14,6 +12,29 @@ const UsersDogs = function (props) {
     }
   });
 
+  const noDogs = (
+    <div className="emptyuser">
+      <p className="nodogs">
+        It appears you don't have any registered dogs yet! Click here to add
+        one!
+      </p>
+      <img src="images/whitedoggo.png" />
+    </div>
+  );
+
+  if (myDogs.length === 0) {
+    return (
+      <div className="emptyuser">
+        <p className="nodogs">
+          It appears you don't have any registered dogs yet! Click  <Link className="register-dog" to="/register-dog">
+            here
+          </Link>{" "} to add
+          one!
+        </p>
+        <img src="images/whitedoggo.png" />
+      </div>
+    );
+  }
   return myDogs.map(dog => {
     return <UserDogProfileCard dog={dog} key={dog.id} />;
   });
