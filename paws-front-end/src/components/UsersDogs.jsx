2b7e7db1600950e26f1613c0c24de777a74018a7
cmaher15 <ceilidhemaher@hotmail.com>
2022-07-19 16:33:49 -0700

    fixed breed select bar

--- paws-front-end/src/components/UsersDogs.jsx
+++ paws-front-end/src/components/UsersDogs.jsx
@@ -30,12 +30,12 @@ const UsersDogs = function (props) {
       <div className="emptyuser">
         <p className="nodogs">
           It appears you don't have any registered dogs yet! Click{" "}
-          <Link className="register-dog" to="/register-dog">
+          <Link className="sign-up" to="/register-dog">
             here
           </Link>{" "}
           to add one! to add one!
         </p>
-        <img src="images/whitedoggo.png" />
+        <img className="waggie" src="images/whitedoggo.png" />
       </div>
     );
   }
